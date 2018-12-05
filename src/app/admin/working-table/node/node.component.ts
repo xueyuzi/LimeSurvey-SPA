8d1f85f9041a3c263c62c178093539b0c954c570
鳕鱼子 <xueyu.d@gmail.com>
2018-12-05 22:52:15 +0800

    node component : add function svg drag

--- src/app/admin/working-table/node/node.component.ts
+++ src/app/admin/working-table/node/node.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import {fromEvent,Subscription} from "rxjs"
 @Component({
-  selector: 'working-node',
+  selector: '[working-node]',
   templateUrl: './node.component.html',
   styleUrls: ['./node.component.scss']
 })
 export class NodeComponent implements OnInit {
-  index = 0;
+  rect:{width:number,height:number,transform?:string}={
+    width:200,
+    height:100,
+  };
+  title:string="node"
+  node:SVGGElement;
+  event:Subscription;
   constructor() { }
-
+  mousemove(e){
+    this.rect.transform = `translate(${e.offsetX-100},${e.offsetY-50})`
+  }
+  mousedown(){
+    this.event = fromEvent<MouseEvent>(window,'mousemove')
+    .subscribe((e)=>this.mousemove(e))
+  }
+  mouseup($event:MouseEvent){
+    this.event.unsubscribe();
+  }
   ngOnInit() {
   }
 
--- src/app/admin/working-table/working-table.service.ts
+++ src/app/admin/working-table/working-table.service.ts
@@ -16,4 +16,6 @@ export class WorkingTableService {
   addNode(){
     this.MainCanvas.append("g")
   }
+
+  
 }
